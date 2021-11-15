927b49e7aa68ad435a7cf891911a57f4b737fd01
guiedington <guilhermeedington@gmail.com>
2021-11-15 12:27:38 -0300

    Criando Model do Produto

---
+++ LojaTitanica/Models/Produto.cs
@@ -0,0 +1,12 @@
+namespace LojaTitanica.Models
+{
+    public class Produto
+    {
+        public int codigo { get; set; }
+        public string nome { get; set; }
+        public string marca { get; set; }
+        public string preco { get; set; }
+        public int quantidade { get; set; }
+    }
+}
+
