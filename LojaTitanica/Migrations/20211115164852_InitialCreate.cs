580bd6a9d34af93f9dd286c79ab7bbe65776b0b7
guiedington <guilhermeedington@gmail.com>
2021-11-15 13:52:30 -0300

    Criando Migration inicial.

---
+++ LojaTitanica/Migrations/20211115164852_InitialCreate.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LojaTitanica.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    marca = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    preco = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    quantidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    clienteid = table.Column<int>(type: "int", nullable: true),
+                    valorTotal = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendas", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Vendas_Clientes_clienteid",
+                        column: x => x.clienteid,
+                        principalTable: "Clientes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    produtoid = table.Column<int>(type: "int", nullable: true),
+                    quantidade = table.Column<int>(type: "int", nullable: false),
+                    Vendaid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Items_Produtos_produtoid",
+                        column: x => x.produtoid,
+                        principalTable: "Produtos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Items_Vendas_Vendaid",
+                        column: x => x.Vendaid,
+                        principalTable: "Vendas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_produtoid",
+                table: "Items",
+                column: "produtoid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_Vendaid",
+                table: "Items",
+                column: "Vendaid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_clienteid",
+                table: "Vendas",
+                column: "clienteid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Vendas");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
