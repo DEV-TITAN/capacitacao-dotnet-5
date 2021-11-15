3fbba08c9d87f876111f272f4872e33b0629d36e
guiedington <guilhermeedington@gmail.com>
2021-11-15 13:47:44 -0300

    Adicionando DbContext no Startup

--- LojaTitanica/Startup.cs
+++ LojaTitanica/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaTitanica.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,8 @@ namespace LojaTitanica
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            string dbConnectionString = Configuration.GetConnectionString("MySQLAppConnection");
+            services.AddDbContext<MySQLContext>(opt => opt.UseMySql(dbConnectionString, ServerVersion.AutoDetect(dbConnectionString)));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
